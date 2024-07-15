77de4f97f8d537d939fab7ee8c70019300612c1e
WaresWichall <jw@trackaligner.com>
2024-07-15 16:19:22 +0100

    Add debug dump script

---
+++ custom_components/anycubic_cloud/scripts/debug_dump_raw.py
@@ -0,0 +1,57 @@
+import argparse
+import asyncio
+import json
+from aiofiles import (
+    open as aio_file_open,
+)
+
+from . import script_base
+
+
+def get_sys_args():
+    parser = argparse.ArgumentParser(description='Anycubic Debug Dump')
+    return vars(parser.parse_args())
+
+
+class anycubic_api_with_script(script_base.anycubic_api_with_script):
+
+    async def script_runner(self):
+        await self.check_api_tokens()
+
+        output_file = 'debug_dump.json'
+
+        user_info = await self.get_user_info(raw_data=True)
+        printer_info = await self.list_my_printers(raw_data=True)
+        projects_info = await self.list_all_projects(raw_data=True)
+        detailed_printer_info = list()
+        if printer_info.get('data') is not None:
+            for printer in printer_info['data']:
+                printer_id = printer['id']
+                detailed_printer_info.append(
+                    await self.printer_info_for_id(
+                        printer_id,
+                        raw_data=True,
+                    )
+                )
+
+        dump = {
+            "user_info": user_info,
+            "printer_info": printer_info,
+            "projects_info": projects_info,
+            "detailed_printer_info": detailed_printer_info,
+        }
+
+        json_dump = json.dumps(dump)
+
+        async with aio_file_open(output_file, mode='w') as f:
+            await f.write(json_dump)
+
+        self._debug_log(f"Dumped to {output_file}")
+
+
+def main():
+    sys_args = get_sys_args()
+    asyncio.run(script_base.main_mqtt_async(anycubic_api_with_script, sys_args))
+
+
+main()
